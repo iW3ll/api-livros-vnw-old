b8ad82bf2efa2d1efef53bccbe51007d586028b0
unknown <wlssilvacontato@gmail.com>
2025-04-22 19:02:41 -0300

    novas atualizações

--- cadastrar_livro.py
+++ cadastrar_livro.py
@@ -2,7 +2,7 @@ import requests
 
 url = "http://localhost:5000/doar"
 dados = {
-    "titulo": "O Senhor dos Anéis",
+    "titulo": "test10",
     "categoria": "Fantasia",
     "autor": "J.R.R. Tolkien",
     "imagem_url": "https://images-americanas.b2w.io/produtos/01/00/img/5202808/2/5202808278_1GG.jpg"
--- cadastrar_livro.sh
+++ cadastrar_livro.sh
@@ -1,7 +1,7 @@
 curl -X POST http://localhost:5000/doar \
 -H "Content-Type: application/json" \
 -d '{
-    "titulo": "Dom Quixote",
+    "titulo": "test10",
     "categoria": "Literatura Clássica",
     "autor": "Miguel de Cervantes",
     "imagem_url": "http://exemplo.com/capa_dom_quixote.jpg"
